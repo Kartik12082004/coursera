d0b77f8d586959e7a647c91c05a980ee0d6ae88c
Kartik12082004 <166854811+Kartik12082004@users.noreply.github.com>
2024-04-13 22:26:28 +0530

    Update SpeakGoodBye.js

--- mod4_sol/SpeakGoodBye.js
+++ mod4_sol/SpeakGoodBye.js
@@ -1,7 +1,10 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
+(function (window){
+    var byeSpeaker = {};
+    var speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
